c603f8482784c764703ecd3aeddae5da3b844aa2
Jakub Cabal <cabal@cesnet.cz>
2022-06-13 12:11:47 +0200

    CARD: [FEATURE] Add changes for QSFP status register support

--- config/user_const.tcl
+++ config/user_const.tcl
@@ -39,6 +39,12 @@ set ETH_PORT_CHAN(0) $env(ETH_PORT_CHAN)
 set ETH_PORT_CHAN(1) $env(ETH_PORT_CHAN)
 set ETH_PORT_CHAN(2) $env(ETH_PORT_CHAN)
 set ETH_PORT_CHAN(3) $env(ETH_PORT_CHAN)
+# Number of lanes for each one of the ETH_PORTS
+# Typical values: 4 (QSFP), 8 (QSFP-DD)
+set ETH_PORT_LANES(0) 4
+set ETH_PORT_LANES(1) 4
+set ETH_PORT_LANES(2) 4
+set ETH_PORT_LANES(3) 4
 # ------------------------------------------------------------------------------
 
 # ==============================================================================
