c7fa819a70b9661c9861c04c01c0468c423f1eba
Jakub Cabal <cabal@cesnet.cz>
2022-10-18 08:39:14 +0200

    CARD: [MAINTENANCE] Reorganization of card parameters

--- config/card_conf.tcl
+++ config/card_conf.tcl
@@ -11,8 +11,40 @@
 set PROJECT_NAME ""
 
 # ------------------------------------------------------------------------------
+# ETH parameters:
+# ------------------------------------------------------------------------------
+# Number of Ethernet ports, must match number of items in list ETH_PORTS_SPEED!
+# This board exists in two variants: FB4CGG3 (with four QSFP ports) and FB2CGG3
+# (with two QSFP). Set the correct number of ETH ports according to your card.
+set ETH_PORTS         $env(ETH_PORTS)
+# Speed for each one of the ETH_PORTS (allowed values: 100)
+# ETH_PORT_SPEED is an array where each index represents given ETH_PORT and
+# each index has associated a required port speed.
+# NOTE: at this moment, all ports must have same speed !
+set ETH_PORT_SPEED(0) $env(ETH_PORT_SPEED)
+set ETH_PORT_SPEED(1) $env(ETH_PORT_SPEED)
+set ETH_PORT_SPEED(2) $env(ETH_PORT_SPEED)
+set ETH_PORT_SPEED(3) $env(ETH_PORT_SPEED)
+# Number of channels for each one of the ETH_PORTS (allowed values: 1 for ETH_PORT_SPEED=100, 4 for ETH_PORT_SPEED<100)
+# ETH_PORT_CHAN is an array where each index represents given ETH_PORT and
+# each index has associated a required number of channels this port has.
+# NOTE: at this moment, all ports must have same number of channels !
+set ETH_PORT_CHAN(0) $env(ETH_PORT_CHAN)
+set ETH_PORT_CHAN(1) $env(ETH_PORT_CHAN)
+set ETH_PORT_CHAN(2) $env(ETH_PORT_CHAN)
+set ETH_PORT_CHAN(3) $env(ETH_PORT_CHAN)
+# Number of lanes for each one of the ETH_PORTS
+# Typical values: 4 (QSFP), 8 (QSFP-DD)
+set ETH_PORT_LANES(0) 4
+set ETH_PORT_LANES(1) 4
+set ETH_PORT_LANES(2) 4
+set ETH_PORT_LANES(3) 4
+
+# ------------------------------------------------------------------------------
 # DMA parameters:
 # ------------------------------------------------------------------------------
+# This variable can be set in COREs *.mk file or as a parameter when launching the make
+set DMA_TYPE             $env(DMA_TYPE)
 # The minimum number of RX/TX DMA channels for this card is 16.
 set DMA_RX_CHANNELS      16
 set DMA_TX_CHANNELS      16
--- config/card_const.tcl
+++ config/card_const.tcl
@@ -24,36 +24,6 @@ set NET_MOD_ARCH "CMAC"
 set SDM_SYSMON_ARCH "USP_IDCOMP"
 
 # ------------------------------------------------------------------------------
-# ETH parameters:
-# ------------------------------------------------------------------------------
-# Number of Ethernet ports, must match number of items in list ETH_PORTS_SPEED!
-# This board exists in two variants: FB4CGG3 (with four QSFP ports) and FB2CGG3
-# (with two QSFP). Set the correct number of ETH ports according to your card.
-set ETH_PORTS         $env(ETH_PORTS)
-# Speed for each one of the ETH_PORTS (allowed values: 100)
-# ETH_PORT_SPEED is an array where each index represents given ETH_PORT and
-# each index has associated a required port speed.
-# NOTE: at this moment, all ports must have same speed !
-set ETH_PORT_SPEED(0) $env(ETH_PORT_SPEED)
-set ETH_PORT_SPEED(1) $env(ETH_PORT_SPEED)
-set ETH_PORT_SPEED(2) $env(ETH_PORT_SPEED)
-set ETH_PORT_SPEED(3) $env(ETH_PORT_SPEED)
-# Number of channels for each one of the ETH_PORTS (allowed values: 1 for ETH_PORT_SPEED=100, 4 for ETH_PORT_SPEED<100)
-# ETH_PORT_CHAN is an array where each index represents given ETH_PORT and
-# each index has associated a required number of channels this port has.
-# NOTE: at this moment, all ports must have same number of channels !
-set ETH_PORT_CHAN(0) $env(ETH_PORT_CHAN)
-set ETH_PORT_CHAN(1) $env(ETH_PORT_CHAN)
-set ETH_PORT_CHAN(2) $env(ETH_PORT_CHAN)
-set ETH_PORT_CHAN(3) $env(ETH_PORT_CHAN)
-# Number of lanes for each one of the ETH_PORTS
-# Typical values: 4 (QSFP), 8 (QSFP-DD)
-set ETH_PORT_LANES(0) 4
-set ETH_PORT_LANES(1) 4
-set ETH_PORT_LANES(2) 4
-set ETH_PORT_LANES(3) 4
-
-# ------------------------------------------------------------------------------
 # PCIe parameters (not all combinations work):
 # ------------------------------------------------------------------------------
 # Supported combinations for this card:
@@ -68,12 +38,6 @@ set PCIE_ENDPOINTS     1
 # PCIe endpoint mode (possible values: 0):
 # 0 = 1x16 lanes
 set PCIE_ENDPOINT_MODE 0
-
-# ------------------------------------------------------------------------------
-# DMA parameters:
-# ------------------------------------------------------------------------------
-# This variable can be set in COREs *.mk file or as a parameter when launching the make
-set DMA_TYPE    $env(DMA_TYPE)
 
 # ------------------------------------------------------------------------------
 # Other parameters:
